124ecfaef81dfbcf31d00bb77a84970838fd3872
Tyler Nielsen <tynielsen@gmail.com>
2025-10-15 15:48:50 -0600

    starting on icons, tweaking styles for light and dark themes

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -4,14 +4,16 @@
  *
 * *****************************************************************************/
 import styles from './Footer.module.css';
-import gitIcon from '/icons/icon-github.svg';
+import GitHub from '/src/assets/icons/icon-github.svg';
 
 function Footer() {
   return(
     <footer id="contact" className={styles.footer}>
       <p>&copy; Copyright {new Date().getFullYear()} - MxTyler</p>
 
-      <a href="https://github.com/tynielsen/mxtyler-reactapp" target="_blank"><img src={gitIcon} width="36" height="36" alt="React Apps GitHub Repository" title="This App's GitHub Repository" /></a>
+      <a href="https://github.com/tynielsen/mxtyler-reactapp" target="_blank">
+        <img src={GitHub} width="36" height="36" style={{color:'purple'}} alt="GitHub Repository" title="This App's GitHub Repository" />
+      </a>
     </footer>
   );
 }
